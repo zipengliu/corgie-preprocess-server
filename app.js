37a8f9e0d4b7f4fff05e454071cb1b537a940bca
Zipeng Liu <zipeng@cs.ubc.ca>
2022-01-11 16:46:23 +0800

    implement function to remove dataset

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const app = express();
 // Add logging middleware
 var accessLogStream = fs.createWriteStream("access.log", { flags: "a" });
 app.use(morgan("combined", { stream: accessLogStream }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // const corsOri = ["http://localhost:3000"];
 // app.use(cors({ origin: corsOri }));
@@ -45,6 +47,7 @@ const fileFields = [
     { name: "featureMeta", maxCount: 1 },
     { name: "predRes", maxCount: 1 },
 ];
+
 const generateID = (req, res, next) => {
     const uuid = crypto.randomBytes(2).toString("hex");
     req.timestamp = Date.now();
@@ -55,6 +58,14 @@ const generateID = (req, res, next) => {
     fs.mkdirSync(req.datasetPath, { recursive: true });
     next();
 };
+
+const isUserDataset = (id) => {
+    if (id.length !== 12 + 1 + 4) return false;
+    const s = id.split("-");
+    if (s.length !== 2 || s[0].length !== 12 || s[1].length !== 4) return false;
+    return true;
+};
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, req.datasetPath);
@@ -79,6 +90,33 @@ app.post("/upload", generateID, uploader.fields(fileFields), (req, res) => {
     });
 
     res.send(req.datasetId);
+});
+
+app.post("/remove", (req, res) => {
+    const id = req.body.id;
+    // Check if it is an user dataset
+    // Note: don't remove a demo dataset
+    console.log("Removing dataset: ", id);
+    if (isUserDataset(id)) {
+        const p = dataPath + id;
+        fs.access(p, (err) => {
+            if (err) {
+                console.error(p + " does not exist!");
+                res.send("0");
+            } else {
+                fs.rmdir(dataPath + id, { recursive: true }, (err) => {
+                    if (err) {
+                        console.error("Error removing " + p);
+                        res.send("0");
+                    } else {
+                        res.send("1");
+                    }
+                });
+            }
+        });
+    } else {
+        res.send("0");
+    }
 });
 
 // app.get("/status", (req, res) => {
